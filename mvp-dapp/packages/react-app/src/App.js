648c2cacd93533d2f6db29bcd8ff0525b65d00e9
Harris <acyclicgraphs@gmail.com>
2023-01-12 10:54:50 -0500

    Update App.js

--- mvp-dapp/packages/react-app/src/App.js
+++ mvp-dapp/packages/react-app/src/App.js
@@ -5,11 +5,57 @@ import IPFS from "ipfs";
 
 import logo from "./ethereumLogo.png";
 import { addresses, abis } from "@project/contracts";
+import { Web3Storage, getFilesFromPath } from "web3.storage";
+
+import { mainnet } from '@filecoin-shipyard/lotus-client-schema?module';
+import { BrowserProvider } from '@filecoin-shipyard/lotus-client-provider-browser?module';
+import { LotusRPC } from '@filecoin-shipyard/lotus-client-rpc?module';
 
 import "./App.css";
 
 const ZERO_ADDRESS =
   "0x0000000000000000000000000000000000000000000000000000000000000000";
+
+const endpointUrl = 'wss://lotus.testground.ipfs.team/api/0/node/rpc/v0'
+
+const provider = new BrowserProvider(endpointUrl)
+
+const client = new LotusRPC(provider, { schema: mainnet.fullNode })
+
+async function run () {
+  try {
+    const version = await client.version()
+    console.log('Version', version)
+  } catch (e) {
+    console.error('client.version error', e)
+  }
+  await client.destroy()
+}
+run()
+
+
+// Lotus client storage commands
+  // lotus client import yourfile.txt
+
+  // creates a DAG with the file and returns the CID
+
+  // make note of the CID
+
+  // lotus client deal
+
+  // lotus client list-deals --show-failed
+
+// Lotus client retrieval commands 
+
+  // FULLNODE_API_INFO=wss://api.chain.love lotus daemon --lite
+
+  // lotus wallet list
+
+  // lotus client retrieve --provider <MINER ID> <PAYLOAD CID> ~/output-file
+  
+
+
+
 
 let node;
 
@@ -20,6 +66,27 @@ const ipfsContract = new ethers.Contract(
   defaultProvider
 );
 
+const learningSessionContract = new ethers.Contract(
+  addresses.learningSession,
+  abis.learningSession,
+  defaultProvider
+);
+
+async function useIpfsLotus() {
+// Start the IPFS daemon on the same machine as your Lotus node:
+// ipfs daemon
+
+// Open ~/.lotus/config.toml and add the following lines:
+// [Client]
+// UseIpfs = true
+
+// Restart your IPFS and Lotus daemon.
+
+// ipfs add -r your_example.txt
+// keep note of the CID
+
+// lotus client deal CID t01000 <price> <duration>
+}
 async function initIpfs() {
   node = await IPFS.create();
   const version = await node.version();
@@ -33,6 +100,16 @@ async function readCurrentUserFile() {
   console.log({ result });
 
   return result;
+}
+
+async function Web3StorageUpload(file) {
+
+const storage = new Web3Storage({ token: process.env.WEB3_TOKEN })
+const files = await getFilesFromPath(process.env.PATH_TO_ADD)
+const cid = await storage.put(files)
+console.log(`IPFS CID: ${cid}`)
+console.log(`Gateway URL: https://dweb.link/ipfs/${cid}`)
+
 }
 
 function App() {
