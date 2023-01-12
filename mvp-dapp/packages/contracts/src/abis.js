91c119ab31f8abc5b0b8d044406e831cba55c62a
Harris <acyclicgraphs@gmail.com>
2023-01-12 11:00:22 -0500

    Update abis.js

--- mvp-dapp/packages/contracts/src/abis.js
+++ mvp-dapp/packages/contracts/src/abis.js
@@ -1,11 +1,13 @@
 import erc20Abi from "./abis/erc20.json";
 import ownableAbi from "./abis/ownable.json";
 import ipfsStorage from "./abis/IpfsStorage.json";
+import learningSession from "./abis/learningSession.json";
 
 const abis = {
   erc20: erc20Abi,
   ownable: ownableAbi,
   ipfs: ipfsStorage,
+  learningSession: learningSession,
 };
 
 export default abis;
