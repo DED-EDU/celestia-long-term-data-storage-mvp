180b32ec51dfc415f29ab47f4ef129cc37129e35
Harris <acyclicgraphs@gmail.com>
2023-01-12 10:59:33 -0500

    Update addresses.js

--- mvp-dapp/packages/contracts/src/addresses.js
+++ mvp-dapp/packages/contracts/src/addresses.js
@@ -1,5 +1,6 @@
 const addresses = {
   ipfs: "0xdc64a140aa3e981100a9beca4e685f962f0cf6c9",
-};
+  learningSession: "0x3befecda6593ed31f47fda97a8b068a899587072",
+};                  
 
 export default addresses;
