4c929fd3d6e908193c01c650a0740e81e4aea83b
Amnot <acyclicgraphs@gmail.com>
2023-03-28 14:49:40 -0400

    Update App.js
    Extracted the ipfs and blockchain logic into separate hooks making the code more modular and maintainable.
    
    Avoided using window.ethereum.enable() as it's deprecated. Instead, using the newer window.ethereum.request({ method: 'eth_requestAccounts' }).
    
    Added error handling for file uploading and contract interactions.
    Use async/await with .map in the loop to ensure proper processing of files (even though the current code only allows one file).
    
    Add a loading state to give feedback to the user while the file is being uploaded to IPFS or the transaction is being processed.

--- mvp-dapp/packages/react-app/src/App.js
+++ mvp-dapp/packages/react-app/src/App.js
@@ -1,83 +1,39 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
-import { ethers } from "ethers";
-import IPFS from "ipfs";
+import { useIpfs } from "./hooks/useIpfs";
+import { useBlockchain } from "./hooks/useBlockchain";
 
 import logo from "./ethereumLogo.png";
-import { addresses, abis } from "@project/contracts";
-
 import "./App.css";
 
-const ZERO_ADDRESS =
-  "0x0000000000000000000000000000000000000000000000000000000000000000";
-
-let node;
-
-const defaultProvider = new ethers.providers.Web3Provider(window.ethereum);
-const ipfsContract = new ethers.Contract(
-  addresses.ipfs,
-  abis.ipfs,
-  defaultProvider
-);
-
-async function initIpfs() {
-  node = await IPFS.create();
-  const version = await node.version();
-  console.log("IPFS Node Version:", version.version);
-}
-
-async function readCurrentUserFile() {
-  const result = await ipfsContract.userFiles(
-    defaultProvider.getSigner().getAddress()
-  );
-  console.log({ result });
-
-  return result;
-}
-
-function App() {
-  const [ipfsHash, setIpfsHash] = useState("");
-  useEffect(() => {
-    initIpfs();
-    window.ethereum.enable();
-  }, []);
-
-  useEffect(() => {
-    async function readFile() {
-      const file = await readCurrentUserFile();
-
-      if (file !== ZERO_ADDRESS) setIpfsHash(file);
-    }
-    readFile();
-  }, []);
-
-  async function setFile(hash) {
-    const ipfsWithSigner = ipfsContract.connect(defaultProvider.getSigner());
-    const tx = await ipfsWithSigner.setFile(hash);
-    console.log({ tx });
-
-    setIpfsHash(hash);
-  }
-
-  const uploadFile = useCallback(async (file) => {
-    const files = [
-      {
-        path: file.name + file.path,
-        content: file,
-      },
-    ];
-
-    for await (const result of node.add(files)) {
-      await setFile(result.cid.string);
-    }
-  }, []);
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const { ipfs, initIpfs } = useIpfs();
+  const { ipfsHash, setFile, readCurrentUserFile } = useBlockchain();
+  const [errorMessage, setErrorMessage] = useState("");
 
   const onDrop = useCallback(
-    (acceptedFiles) => {
-      uploadFile(acceptedFiles[0]);
+    async (acceptedFiles) => {
+      setLoading(true);
+      setErrorMessage("");
+      try {
+        const files = acceptedFiles.map((file) => ({
+          path: file.name + file.path,
+          content: file,
+        }));
+
+        for await (const result of ipfs.add(files)) {
+          await setFile(result.cid.toString());
+        }
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setLoading(false);
+      }
     },
-    [uploadFile]
+    [ipfs, setFile]
   );
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     multiple: false,
     onDrop,
@@ -92,13 +48,11 @@ function App() {
           {isDragActive ? (
             <p>Drop the files here ...</p>
           ) : (
-            <p>
-              Click the logo to upload to IPFS
-            </p>
+            <p>Click the logo to upload to IPFS</p>
           )}
         </div>
         <div>
-          {ipfsHash !== "" ? (
+          {ipfsHash ? (
             <a
               href={`https://ipfs.io/ipfs/${ipfsHash}`}
               target="_blank"
@@ -110,9 +64,11 @@ function App() {
             "No user file set yet"
           )}
         </div>
+        {loading && <div>Uploading file...</div>}
+        {errorMessage && <div className="error">{errorMessage}</div>}
       </header>
     </div>
   );
-}
+};
 
 export default App;
