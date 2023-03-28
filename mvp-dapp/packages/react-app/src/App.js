8d5d5a8fd93bcd3fc47f340ab7b8fd94779d4f6d
Amnot <acyclicgraphs@gmail.com>
2023-03-28 14:59:48 -0400

    Update App.js

--- mvp-dapp/packages/react-app/src/App.js
+++ mvp-dapp/packages/react-app/src/App.js
@@ -1,18 +1,26 @@
 import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
-import { useIpfs } from "./hooks/useIpfs";
-import { useBlockchain } from "./hooks/useBlockchain";
-
-import logo from "./ethereumLogo.png";
-import spinner from "./spinner.gif";
+import { ethers } from "ethers";
+import LearningSession from "./artifacts/LearningSession.json"; // Import ABI
 import "./App.css";
+
+// Replace with your deployed contract address
+const contractAddress = "0x1234...";
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const { ipfs, initIpfs } = useIpfs();
-  const { ipfsHash, setFile, readCurrentUserFile } = useBlockchain();
   const [errorMessage, setErrorMessage] = useState("");
+  const [ipfsHash, setIpfsHash] = useState("");
+  const [artifactId, setArtifactId] = useState(""); // Artifact ID for adding comments
+  const [voteArtifactId, setVoteArtifactId] = useState(""); // Artifact ID for voting
+  const [voteValue, setVoteValue] = useState(0); // Vote value: -1, 0, or 1
 
+  // Initialize contract instance
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const learningSessionContract = new ethers.Contract(contractAddress, LearningSession.abi, signer);
+
+  // Handle file upload (for adding session artifact or comment)
   const onDrop = useCallback(
     async (acceptedFiles) => {
       setLoading(true);
@@ -24,7 +32,14 @@ const App = () => {
         }));
 
         for await (const result of ipfs.add(files)) {
-          await setFile(result.cid.toString());
+          setIpfsHash(result.cid.toString());
+          if (!artifactId) {
+            // Add a new session artifact
+            await learningSessionContract.addSessionArtifact(result.cid.toString());
+          } else {
+            // Add a comment to an existing artifact
+            await learningSessionContract.addComment(artifactId, result.cid.toString());
+          }
         }
       } catch (error) {
         setErrorMessage(error.message);
@@ -32,8 +47,22 @@ const App = () => {
         setLoading(false);
       }
     },
-    [ipfs, setFile]
+    [ipfs, artifactId, learningSessionContract]
   );
+
+  // Handle vote submission
+  const submitVote = async () => {
+    setLoading(true);
+    setErrorMessage("");
+    try {
+      // Submit the vote to the smart contract
+      await learningSessionContract.vote(voteArtifactId, voteValue);
+    } catch (error) {
+      setErrorMessage(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     multiple: false,
@@ -43,39 +72,42 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <div {...getRootProps()} className="dropzone">
-          <input {...getInputProps()} />
+        <div {...getRootProps()} style={{ cursor: "pointer" }}>
           <img src={logo} className="App-logo" alt="react-logo" />
+          <input {...getInputProps()} />
           {isDragActive ? (
-            <p>Drop the file here to upload to IPFS</p>
+            <p>Drop the files here ...</p>
           ) : (
-            <p>Click or drag a file here to upload to IPFS</p>
+            <p>Click the logo to upload to IPFS</p>
           )}
         </div>
-        {loading ? (
-          <div>
-            <img src={spinner} className="spinner" alt="Loading" />
-            <p>Uploading file...</p>
-          </div>
-        ) : (
-          <div className="file-info">
-            {ipfsHash ? (
-              <a
-                href={`https://ipfs.io/ipfs/${ipfsHash}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                See current user file
-              </a>
-            ) : (
-              <p>No user file set yet</p>
-            )}
-          </div>
-        )}
-        {errorMessage && <div className="error">{errorMessage}</div>}
-      </header>
-    </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Artifact ID for comment (optional)"
+            value={artifactId}
+            onChange={(e) => setArtifactId(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Artifact ID for vote"
+            value={voteArtifactId}VoteArtifactId(e.target.value)}
+          />
+      <select value={voteValue} onChange={(e) => setVoteValue(parseInt(e.target.value))}>
+      <option value={-1}>-1 (Downvote)</option>
+      <option value={0}>0 (Neutral)</option>
+      <option value={1}>1 (Upvote)</option>
+    </select>
+  <button onClick={submitVote}>Submit Vote</button>
+  </div>
+  {loading && <div>Loading...</div>}
+    {errorMessage && <div className="error">{errorMessage}</div>}
+  </header>
+  </div>
   );
 };
 
 export default App;
+            onChange={(e) => set
