a35b99af2c67b1efa71ccbe81023fb0d1e01275f
Amnot <acyclicgraphs@gmail.com>
2023-03-28 14:52:37 -0400

    Update useIpfs.js

--- mvp-dapp/packages/react-app/src/hooks/useIpfs.js
+++ mvp-dapp/packages/react-app/src/hooks/useIpfs.js
@@ -1,1 +1,44 @@
+import { useCallback, useEffect, useState } from "react";
+import IPFS from "ipfs";
+
+const useIpfs = () => {
+  const [node, setNode] = useState(null);
+  const [ipfsReady, setIpfsReady] = useState(false);
+
+  useEffect(() => {
+    const initIpfs = async () => {
+      const ipfsNode = await IPFS.create();
+      setNode(ipfsNode);
+      setIpfsReady(true);
+    };
+    initIpfs();
+  }, []);
+
+  const uploadFile = useCallback(async (file) => {
+    if (!ipfsReady || !node) {
+      console.error("IPFS node is not ready.");
+      return null;
+    }
+    try {
+      const files = [
+        {
+          path: file.name + file.path,
+          content: file,
+        },
+      ];
+      let resultCid;
+      for await (const result of node.add(files)) {
+        resultCid = result.cid.toString();
+      }
+      return resultCid;
+    } catch (error) {
+      console.error("Failed to upload file to IPFS:", error);
+      return null;
+    }
+  }, [ipfsReady, node]);
+
+  return { ipfsReady, uploadFile };
+};
+
+export default useIpfs;
 
